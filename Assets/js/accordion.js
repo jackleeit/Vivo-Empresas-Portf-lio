d18f93e1ba0f802c04a5ff2c07b2b20cf477ddbe
Jack Lee <jackleeit@googlemail.com>
2012-09-20 20:16:56 -0300

    ajustes js

--- Assets/js/accordion.js
+++ Assets/js/accordion.js
@@ -1,133 +1,147 @@
-Math.easeInQuad = function (t, b, c, d) {
-	return c*(t/=d)*t + b;
+Math.easeInQuad = function(t, b, c, d) {
+	return c * (t /= d) * t + b;
 };
 
-$(document).ready(
-		function() {
-			
-			var accordion = (function() {
+$(document)
+		.ready(
+				function() {
 
-				var $currentItem,
-				
-				interval,
-				
-				maxAvailableHeight = 584, 
+					var accordion = (function() {
 
-				itemOnClass = "topicOn",
+						var $currentItem,
 
-				accordionContent = ".accordionContent",
+						interval,
 
-				animationConfig = {
-					duration : 900,
-					easing : 'swing'
-				},
+						maxAvailableHeight = 584,
 
-				showItem = function(event) {
+						itemOnClass = "topicOn",
 
-					event.preventDefault();
+						accordionContent = ".accordionContent",
 
-					$currentItem = $(this);
+						animationConfig = {
+							duration : 900,
+							easing : 'swing'
+						},
 
-					showContent();
-				},
+						showItem = function(event) {
 
-				showContent = function() {
-					if ( ! $currentItem.hasClass(itemOnClass)) {						
-						$currentItem.addClass(itemOnClass).parent().next(
-								accordionContent).slideDown(
-								animationConfig.duration,
-								animationConfig.easing, showContentCallback);
-						
-						var $h3 = $currentItem.parent();
-						
-						if (myScroll && ($h3.get(0).offsetTop > maxAvailableHeight))
-						{
-							var intervalo;
-							var t = new Date();
-							function createTween(begin, finish, duration, funcao)
-							{
-								duration = duration * 32;
-								var _c;
-								var change = finish - begin;
-								var time = 0;
-								intervalo = setInterval(function() {
-									_c = funcao(time++, begin, change, duration);
-									 myScroll.scrollTo(0, _c * -1, 5);
-									if (time > duration) clearInterval(intervalo);
-								}, 33);
+							event.preventDefault();
+
+							$currentItem = $(this);
+
+							showContent();
+						},
+
+						showContent = function() {
+							if (!$currentItem.hasClass(itemOnClass)) {
+								$currentItem.addClass(itemOnClass).parent()
+										.next(accordionContent).slideDown(
+												animationConfig.duration,
+												animationConfig.easing,
+												showContentCallback);
+
+								var $h3 = $currentItem.parent();
+
+								if (myScroll
+										&& ($h3.get(0).offsetTop > maxAvailableHeight)) {
+									var intervalo;
+									var t = new Date();
+									function createTween(begin, finish,
+											duration, funcao) {
+										duration = duration * 32;
+										var _c;
+										var change = finish - begin;
+										var time = 0;
+										intervalo = setInterval(function() {
+											_c = funcao(time++, begin, change,
+													duration);
+											myScroll.scrollTo(0, _c * -1, 5);
+											if (time > duration)
+												clearInterval(intervalo);
+										}, 33);
+									}
+									createTween($h3.get(0).offsetTop, $h3
+											.data('originalTopOffset'), 1,
+											Math.easeInQuad);
+									/*
+									 * var now = 0; var end = 100; var step =
+									 * 50; var fim =
+									 * $h3.data('originalTopOffset'); var inicio =
+									 * $h3.get(0).offsetTop; var diferencia =
+									 * inicio - fim;
+									 * 
+									 * var interval = setInterval(function(){
+									 * step--;
+									 * 
+									 * now = (((end/100) * step)); bounce = (now -
+									 * (now/21));
+									 * 
+									 * myScroll.scrollTo(0, ((((diferencia /
+									 * 100) * bounce) + fim)-10) * -1, 5);
+									 * 
+									 * if(step <= 0) clearTimeout(interval); },
+									 * 20) }
+									 */
+								} else {
+									$currentItem.parent()
+											.next(accordionContent).slideUp(
+													animationConfig.duration,
+													animationConfig.easing,
+													hideContentCallback);
+								}
+
+								hideContent();
 							}
-							createTween($h3.get(0).offsetTop, $h3.data('originalTopOffset'), 1, Math.easeInQuad);													
-							/*var now = 0;
-							var end = 100;
-							var step = 50;
-							var fim = $h3.data('originalTopOffset');
-							var inicio = $h3.get(0).offsetTop;
-							var diferencia = inicio - fim;
+						},
 
-							var interval = setInterval(function(){
-							    step--;
+						hideContent = function() {
 
-							    now = (((end/100) * step));
-							    bounce = (now - (now/21));
+							$currentItem.parent().siblings('h3').each(
+									function() {
 
-							    myScroll.scrollTo(0, ((((diferencia / 100) * bounce) + fim)-10) * -1, 5);
+										$(this).next(accordionContent).slideUp(
+												animationConfig.duration,
+												animationConfig.easing,
+												function() {
+													setTimeout(function() {
+													}, 0);
 
-							    if(step <= 0)
-							       clearTimeout(interval);
-							}, 20)
-						}*/
-					} else {						
-						$currentItem.parent().next(accordionContent).slideUp(
-								animationConfig.duration,
-								animationConfig.easing, hideContentCallback);
-					}					
+												});
 
-					hideContent();						
-				},
+										$(this).find('a').removeClass(
+												itemOnClass);
+									});
+						},
 
-				hideContent = function() {
+						showContentCallback = function() {
 
-					$currentItem.parent().siblings('h3').each(
-							function() {
+							setTimeout(function() {
+								if (myScroll) {
+									myScroll.refresh();
+								}
+							}, 0);
+						},
 
-								$(this).next(accordionContent).slideUp(
-										animationConfig.duration,
-										animationConfig.easing, function() {
-											setTimeout(function() {
-											}, 0);
+						hideContentCallback = function() {
+							$currentItem.removeClass(itemOnClass);
+							setTimeout(function() {
+								/*
+								 * if (myScroll) { myScroll.refresh(); }
+								 */
+							}, 0);
+						};
 
-										});
+						$("#accordion").delegate('h3 a', 'click', showItem);
 
-								$(this).find('a').removeClass(itemOnClass);
-							});
-				},
-
-				showContentCallback = function() {		
-					
-					setTimeout(function(){
-						if (myScroll)
-						{
-							myScroll.refresh();
-						}                
-					}, 0);
-				},
-
-				hideContentCallback = function() {					
-					$currentItem.removeClass(itemOnClass);
-					setTimeout(function(){
-						/*if (myScroll)
-						{
-							myScroll.refresh();
-						} */
-					}, 0);
-				};
-				
-				$("#accordion").delegate('h3 a', 'click', showItem);
-				
-				$("#accordion h3").each(function(){
-					$(this).data('originalTopOffset', this.offsetTop);
-					$(this).data('originalLeftOffset', this.offsetLeft);
-					//console.log($(this).data('originalTopOffset') + "  ;  " + $(this).data('originalLeftOffset'));
-				});				
-			})();
-		});
+						$("#accordion h3").each(
+								function() {
+									$(this).data('originalTopOffset',
+											this.offsetTop);
+									$(this).data('originalLeftOffset',
+											this.offsetLeft);
+									// console.log($(this).data('originalTopOffset')
+									// + " ; " +
+									// $(this).data('originalLeftOffset'));
+								});
+					})();
+				});

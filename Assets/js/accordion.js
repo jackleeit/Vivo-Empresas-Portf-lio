889f08eb570331322e85f8562222f113ddcfe339
Jack Lee <jackleeit@googlemail.com>
2012-09-20 16:21:40 -0300

    Ajustes JS

--- Assets/js/accordion.js
+++ Assets/js/accordion.js
@@ -1,9 +1,11 @@
 $(document).ready(
 		function() {
+			
 			var accordion = (function() {
 
 				var $currentItem,
-				interval,
+				
+				maxAvailableHeight = 584, 
 
 				itemOnClass = "topicOn",
 
@@ -24,11 +26,18 @@ $(document).ready(
 				},
 
 				showContent = function() {
-					if (!$currentItem.hasClass(itemOnClass)) {						
+					if ( ! $currentItem.hasClass(itemOnClass)) {						
 						$currentItem.addClass(itemOnClass).parent().next(
 								accordionContent).slideDown(
 								animationConfig.duration,
-								animationConfig.easing, showContentCallback);								
+								animationConfig.easing, showContentCallback);
+						
+						var $h3 = $currentItem.parent();
+						
+						if (myScroll && ($h3.get(0).offsetTop > maxAvailableHeight))
+						{							
+							myScroll.scrollTo(0, $h3.data('originalTopOffset'), 5);							
+						}
 					} else {						
 						$currentItem.parent().next(accordionContent).slideUp(
 								animationConfig.duration,
@@ -37,11 +46,11 @@ $(document).ready(
 
 					hideContent();
 					
-					if (myScroll)
+					/*if (myScroll)
 					{
 						var elem = $currentItem.get(0);
 						myScroll.scrollTo(elem.offsetLeft, elem.offsetTop, 5);
-					}					
+					}*/			
 				},
 
 				hideContent = function() {
@@ -84,8 +93,13 @@ $(document).ready(
 						} */
 					}, 0);
 				};
-
+				
 				$("#accordion").delegate('h3 a', 'click', showItem);
-
+				
+				$("#accordion h3").each(function(){
+					$(this).data('originalTopOffset', this.offsetTop);
+					$(this).data('originalLeftOffset', this.offsetLeft);
+					//console.log($(this).data('originalTopOffset') + "  ;  " + $(this).data('originalLeftOffset'));
+				});				
 			})();
 		});
